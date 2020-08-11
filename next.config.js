dadfc957277aed1c0c6c9cbdb02897f3a2108cda
Logan Wishart-Craig <loganwishartcraig@gmail.com>
2020-08-11 14:39:45 -0700

    Initial NetlifyCMS commit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    webpack: (cfg) => {
+
+        cfg.module.rules.push(
+            {
+                test: /\.md$/,
+                loader: 'frontmatter-markdown-loader',
+                options: { mode: ['react-component'] }
+            }
+        );
+
+        return cfg;
+
+    }
+}
